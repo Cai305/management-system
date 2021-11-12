272df03605d6e75492812164aa4738d1763db34c
caiphus <caiphus@thedigitalacademy.co.za>
2021-11-12 07:56:07 +0200

    Primeng installed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,11 +20,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'management'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('management');
-  });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,9 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 
 import { AutoCompleteModule } from "primeng/autocomplete";
-import { CalendarModule } from "primeng/calendar";
-import { ChipsModule } from "primeng/chips";
-import { DropdownModule } from "primeng/dropdown";
-import { InputMaskModule } from "primeng/inputmask";
-import { InputNumberModule } from "primeng/inputnumber";
-import { CascadeSelectModule } from "primeng/cascadeselect";
-import { MultiSelectModule } from "primeng/multiselect";
-import { InputTextareaModule } from "primeng/inputtextarea";
-import { InputTextModule } from "primeng/inputtext";
+import { ButtonModule } from "primeng/button";
+import { RippleModule } from "primeng/ripple";
+
 
 @NgModule({
   imports: [
@@ -24,15 +18,9 @@ import { InputTextModule } from "primeng/inputtext";
     AutoCompleteModule,
     FormsModule,
     HttpClientModule,
-    CalendarModule,
-    ChipsModule,
-    DropdownModule,
-    InputMaskModule,
-    InputNumberModule,
-    CascadeSelectModule,
-    MultiSelectModule,
-    InputTextareaModule,
-    InputTextModule
+    ButtonModule,
+    RippleModule
+    
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
